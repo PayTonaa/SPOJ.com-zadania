3f98a66ca5f06f653270c6b83d41717760ae661a
Bartek <139969478+PayTonaa@users.noreply.github.com>
2023-07-19 13:06:35 +0200

    Create czy_nieparzysta_liczba_dzielnikow.py
    https://pl.spoj.com/problems/FR_14_08/

---
+++ czy_nieparzysta_liczba_dzielnikow.py
@@ -0,0 +1,33 @@
+def czy_nieparzysta_liczba_dzielnikow(n):
+    dzielniki = 0
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            dzielniki += 1
+            if i != n // i:
+                dzielniki += 1
+
+    return dzielniki % 2 != 0
+
+
+n = int(input())
+
+
+if czy_nieparzysta_liczba_dzielnikow(n):
+    print("TAK")
+else:
+    print("NIE")
+
+
+
+# polecenie:
+
+
+# FR_14_08 - Nieparzysta liczba dzielników
+
+# Napisz program, który sprawdzi, czy liczba ma nieparzystą liczbę dzielników.
+
+# Wejście
+# Pierwszy i jedyny wiersz wejścia zawiera jedną liczbę całkowitą n (1 ≤ n ≤ 109).
+
+# Wyjście
+# W pierwszym i jedynym wierszu wyjścia należy 
